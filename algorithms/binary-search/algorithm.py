d66b3253615a6a0f8e05f259df90e9a5841d8146
Martinson <Peter.Martinson@christianacare.org>
2022-06-03 14:54:51 -0400

    results of black

--- algorithms/binary-search/algorithm.py
+++ algorithms/binary-search/algorithm.py
@@ -1,21 +1,25 @@
-
 def search(input_list, target):
     nums = sorted(input_list)
     print(nums)
 
-    (left, right) = (0, len(nums) - 1) # simultaneous assignment of left and right bounds
+    (left, right) = (
+        0,
+        len(nums) - 1,
+    )  # simultaneous assignment of left and right bounds
 
     while left <= right:
 
-        mid = (left + right) // 2 # rounds down if it's a decimal
+        mid = (left + right) // 2  # rounds down if it's a decimal
 
-        if target == nums[mid]: # if it finds the target, return the current index
+        if (
+            target == nums[mid]
+        ):  # if it finds the target, return the current index
             return mid
 
-        elif target < nums[mid]: # Target is smaller, discard the right
+        elif target < nums[mid]:  # Target is smaller, discard the right
             right = mid - 1
 
-        else:                    # Target is larger, discard the left
+        else:  # Target is larger, discard the left
             left = mid + 1
 
     return None
